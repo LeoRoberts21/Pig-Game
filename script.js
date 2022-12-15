301d06ab6b6e9520f58342af21290f213a2b2b59
Leo Roberts <100042123+LeoRoberts21@users.noreply.github.com>
2022-12-15 22:53:20 +0000

    design changes

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ btnHold.addEventListener("click", function () {
       scores[activePlayer];
 
     // 2. Check if score is >= 100
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= 20) {
 
       // Finish game
       playing = false;
